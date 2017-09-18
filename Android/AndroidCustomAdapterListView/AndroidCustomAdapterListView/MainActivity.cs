cf981ed4693654fa76e8a49ed70a52ed764f306a
glitcyneudesic <30427667+glitcyneudesic@users.noreply.github.com>
2017-09-18 16:55:11 +0530

    Custom Adapter
    Created a custom adapter

---
+++ Android/AndroidCustomAdapterListView/AndroidCustomAdapterListView/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using AndroidCustomAdapterListView.Resources;
+
+namespace AndroidCustomAdapterListView
+{
+    [Activity(Label = "AndroidCustomAdapterListView", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var lstdata = FindViewById<ListView>(Resource.Id.listView);
+            var btnshow = FindViewById<Button>(Resource.Id.btnShow);
+            btnshow.Click += delegate
+            {
+
+                List<Person> lstSource = new List<Person>();
+                for (int i = 0; i < 20; i++)
+                {
+                    Person per = new Person()
+                    {
+                        Id = i,
+                        Name = "name" + i,
+                        Age = 2,
+                        Email = "email" + i
+                    };
+                    lstSource.Add(per);
+                }
+                var adapter = new CustomAdapter(this, lstSource);
+                lstdata.Adapter = adapter;
+
+            };
+
+        }
+    }
+}
+
---
+++ Android/AndroidCustomAdapterListView/AndroidCustomAdapterListView/Resources/CustomAdapter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace AndroidCustomAdapterListView.Resources
+{
+    public class ViewHolder : Java.Lang.Object
+    {
+        public TextView txtName { get; set; }
+        public TextView txtAge { get; set; }
+        public TextView txtEmail { get; set; }
+    }
+  public  class CustomAdapter:BaseAdapter
+    {
+
+        private Activity activity;
+        private List<Person> persons;
+        public CustomAdapter(Activity activity,List<Person> persons)
+        {
+            this.activity = activity;
+            this.persons = persons;
+        }
+        public override int Count
+        {
+            get
+            {
+                return persons.Count;
+            }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return persons[position].Id;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView ?? activity.LayoutInflater.Inflate(Resource.Layout.listtwo, parent, false);
+            var txtName = view.FindViewById<TextView>(Resource.Id.textView1);
+            var txtAge = view.FindViewById<TextView>(Resource.Id.textView2);
+            var txtEmail = view.FindViewById<TextView>(Resource.Id.textView3);
+
+            txtName.Text = persons[position].Name;
+            txtAge.Text = persons[position].Age.ToString();
+            txtEmail.Text = persons[position].Email;
+
+            return view;
+        }
+    }
+}
