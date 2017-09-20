7edad1faaebeae922b2aa95a24ccf497850a4483
glitcyneudesic <30427667+glitcyneudesic@users.noreply.github.com>
2017-09-20 19:41:49 +0530

    New files

---
+++ MVVMForm/FormMVVMDemo/FormMVVMDemo/FormMVVMDemo/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FormMVVMDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var quoteViewModel = new QuoteViewModel();
+
+
+            var label = new Entry();
+            label.SetBinding(Entry.TextProperty, "QuoteName",BindingMode.TwoWay);
+            label.HorizontalOptions = LayoutOptions.Center;
+
+            var button = new Button();
+            button.SetBinding(Button.CommandProperty, "ResetQuoteName");
+            button.Text = "Reset Quote in code";
+
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        label,
+                        new QuoteView(),
+                        button
+                        //new Label
+                        //{
+                        //    XAlign=TextAlignment.Center,
+                        //    Text = "Welcome Glitz"
+                        //}
+                    }
+                },
+                BindingContext=quoteViewModel
+            };
+            //MainPage = new FormMVVMDemo.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MVVMForm/FormMVVMDemo/FormMVVMDemo/FormMVVMDemo/QuoteViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormMVVMDemo
+{
+    public class QuoteViewModel:INotifyPropertyChanged
+    {
+        public QuoteViewModel()
+        {
+            QuoteName = "Glitcy";
+        }
+        string _quoteName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string QuoteName
+        {
+            get
+            {
+                return _quoteName;
+            }
+            set
+            {
+                _quoteName = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("QuoteName"));
+                }
+            }
+        }
+            public Command  ResetQuoteName
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    QuoteName = "Glitcy";
+                });
+            }
+        
+        }
+    }
+}
